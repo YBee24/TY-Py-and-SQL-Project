9edbc2dd2bf427f03d3c2e781721aabf38e00cc4
inception-tino <121115415+inception-tino@users.noreply.github.com>
2024-08-16 01:05:27 +0100

    Add files via upload

---
+++ kora/symphonic_kora_library/app/config.py
@@ -0,0 +1,10 @@
+import mysql.connector
+
+# This function opens the box and lets us put things in or take things out.
+def create_connection():
+    return mysql.connector.connect(
+        host="localhost",  # The name of the box
+        user="root",  # The key to open the box (root user)
+        password="root",  # The secret code for the box (replace with your MySQL password)
+        database="symphonic_kora_library"  # The specific box we want to play with
+    )
